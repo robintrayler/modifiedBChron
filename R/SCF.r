6c866c66c23c6cb0d8aef3562e648f10789ee016
robintrayler <robintrayler@gmail.com>
2018-11-09 14:09:43 -0700

    added example data

---
+++ R/SCF.r
@@ -0,0 +1,11 @@
+#' Mixture of U-Pb and 40Ar/39Ar dates from the Santa Cruz Formation, Argentina
+#' @format A data frame with 38 individual dates from 9 different volcanic tuffs
+#' \describe{
+#' \item{ids} Tuff name for each sample
+#' \item{age} U-Pb or 40Ar/39Ar age for each sample
+#' \item{ageSds} 1 sigma uncertainty
+#' \item{position} stratigraphic position in meters
+#' \item{thickness} stratigraphic thickness uncertainty
+#' \item{distType} type of distribution to model each sample as
+#' }
+#' "SCF"
