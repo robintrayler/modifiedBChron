94a7893ba378c31bd3a4aa97ef2ee20daf465125
robintrayler <robintrayler@gmail.com>
2019-06-07 16:43:01 -0600

    added probability specification

--- R/ageExtract.r
+++ R/ageExtract.r
@@ -3,15 +3,15 @@
 #' @return HDI = specified probability highest density interval of the posterior distribution for each input horizon
 #'
 #' @export
-ageExtract <- function(model){
+ageExtract <- function(model, probability = 0.95){
   HDI <- t(apply(X = model$thetas,
                  MARGIN = 2,
                  FUN = quantile,
-                 prob = c((1 - model$probability)/2, 0.5, (1 + model$probability)/2)))
+                 prob = c((1 - probability)/2, 0.5, (1 + probability)/2)))
   extract <- data.frame(ids = model$ids, HDI = as.matrix(HDI))
   colnames(extract) <- c('ids',
-                         paste((1 - model$probability)/2*100, '%', sep = ''),
+                         paste((1 - probability)/2*100, '%', sep = ''),
                          paste(50, '%', sep = ''),
-                         paste((1 + model$probability)/2*100, '%', sep = ''))
+                         paste((1 + probability)/2*100, '%', sep = ''))
   return(HDI = extract)
 }
