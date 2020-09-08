1608b17a8a1bfd885d20bba43da0a8d353612e24
robintrayler <robintrayler@gmail.com>
2020-09-07 20:17:57 -0700

    fixed uniform distribution bug

--- R/sumPDF.R
+++ R/sumPDF.R
@@ -5,42 +5,49 @@
 #' @param distTypes Vector of distribution types to model each age as. Choices are 'G' for Gaussian, and 'U' uniform. Must be the same length and given in the same order as \code{ages}
 #' @param ids Vector of sample names for each age. All samples with the same ids will be combined into a single age PDF. Must be the same length and given in the same order as \code{ages}
 #' @export
-
+#'
 sumPDF <- function(ages,
                    ageSds,
                    distTypes = rep('G',length(ages)),
                    ids){
+
   samples <- unique(ids)
-  n <- length(samples)
+  n_samples <- length(samples)
   ageGrid <- list()
   densities <- list()
   out <- list()
-  for(i in 1:n){
-    ageGrid[[i]] <- seq(min(ages[ids == samples[i]] - ageSds[ids == samples[i]]*6),
-                       max(ages[ids == samples[i]] + ageSds[ids == samples[i]]*6), length = 100000)
-    interval <- matrix(0,nrow = 100000, ncol = length(ages[ids == samples[i]]))
 
-    for(j in 1:length(ages[ids == samples[i]])){
-      if(distTypes[j] == 'G'){
-        interval[, j] <- dnorm(ageGrid[[i]],
-                               mean = ages[ids == samples[i]][j],
-                              sd = ageSds[ids == samples[i]][j])/
-          length(ages[ids == samples[i]][j])*mean(diff(ageGrid[[i]]))
-      }
-      else if(distTypes[j] == 'U'){
-        interval[, j] <- dunif(ageGrid[[i]],
-                              min = ages[ids == samples[i]][j] - ageSds[ids == samples[i]][j],
-                              max = ages[ids == samples[i]][j] + ageSds[ids == samples[i]][j])/
-          length(ages[ids == samples[i]])*mean(diff(ageGrid[[i]]))
-      }
+  for(i in 1:n_samples) {
+    ageGrid[[i]] <- seq(min(ages[ids == samples[i]] - ageSds[ids == samples[i]] * 10),
+                        max(ages[ids == samples[i]] + ageSds[ids == samples[i]] * 10),
+                        length = 100000)
+    interval <- matrix(0,
+                       nrow = 100000,
+                       ncol = length(ages[ids == samples[i]]))
+    current_n <- length(ages[ids == samples[i]])
+    current_ages <- ages[ids == samples[i]]
+    current_sds <- ageSds[ids == samples[i]]
+    current_dists <- distTypes[ids == samples[i]]
 
+    for(k in 1:current_n) {
+      if(current_dists[k] == 'G') {
+        interval[, k] <- dnorm(x = ageGrid[[i]],
+                               mean = current_ages[k],
+                               sd = current_sds[k])
+
+      }
+      else if(current_dists[k] == 'U') {
+        interval[, k] <- dunif(x = ageGrid[[i]],
+                               min = current_ages[k] - current_sds[k],
+                               max = current_ages[k] + current_sds[k])
+      }
     }
+
     densities[[i]] <- base::apply(interval, 1, sum)
     out[[i]] <- data.frame(ageGrid = ageGrid[[i]],
-                     densities = densities[[i]])
-
+                           densities = densities[[i]])
   }
+
   names(out) <- samples
   return(out)
 }
-
